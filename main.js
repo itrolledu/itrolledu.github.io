1398b1ee502b934963b721aa1fd9623132bcaa02
Mason X <ohnx@users.noreply.github.com>
2014-11-15 22:20:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function setLink(url) {
   	$("#siteloader").html('<object data="'+url+'" height="90%" width="100%">');
 }
-http://itrolledu.github.io/mac/index.html
+$('#home').click(function(){ setLink("http://itrolledu.github.io/main.html"); return false; });
 $('#mac').click(function(){ setLink("http://itrolledu.github.io/mac/index.html"); return false; });
 $('#win').click(function(){ setLink("http://itrolledu.github.io/win/index.html"); return false; });
-$('#linux').click(function(){ setLink("http://itrolledu.github.io/"); return false; });
-$('#about').click(function(){ setLink("http://itrolledu.github.io/"); return false; });
-$('#help').click(function(){ setLink("http://itrolledu.github.io/"); return false; });
+$('#linux').click(function(){ setLink("http://itrolledu.github.io/linux/index.html"); return false; });
+$('#about').click(function(){ setLink("http://itrolledu.github.io/about.html"); return false; });
+$('#help').click(function(){ setLink("http://itrolledu.github.io/help.html"); return false; });
 $("#siteloader").html('<object data="http://itrolledu.github.io/main.html" height="90%" width="100%">');
