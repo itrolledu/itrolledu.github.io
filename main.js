942d88bf1ae2442127f3d4d1c6cc24e334d3fbf8
Mason X <ohnx@users.noreply.github.com>
2014-11-15 21:09:13 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function setLink(url) {
+  	$("#siteloader").html('<object data="'+url+'" height="100%" width="100%">');
+}
+http://itrolledu.github.io/mac/index.html
+$('#mac').click(function(){ setLink("http://itrolledu.github.io/mac/index.html"); return false; });
+$('#win').click(function(){ setLink("http://itrolledu.github.io/win/index.html"); return false; });
+$('#linux').click(function(){ setLink("http://itrolledu.github.io/"); return false; });
+$('#about').click(function(){ setLink("http://itrolledu.github.io/"); return false; });
+$('#help').click(function(){ setLink("http://itrolledu.github.io/"); return false; });
+$("#siteloader").html('<object data="http://itrolledu.github.io/main.html" height="100%" width="100%">');
