9a504da8b4ca168f2340cad691e7f96aeb709e34
Mason X <ohnx@users.noreply.github.com>
2014-11-18 21:17:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ $.ajax({
     url: page,
     dataType: "jsonp",
 }).success( function( data ) {
-    $( 'div.siteloader' ).html( data );
+    $( '#siteloader' ).html( data );
 });
 }
-$('#home').click(function(){ setLink("http://itrolledu.github.io/main.html"); return false; });
-$('#mac').click(function(){ setLink("http://itrolledu.github.io/mac/index.html"); return false; });
-$('#win').click(function(){ setLink("http://itrolledu.github.io/win/index.html"); return false; });
-$('#linux').click(function(){ setLink("http://itrolledu.github.io/linux/index.html"); return false; });
-$('#about').click(function(){ setLink("http://itrolledu.github.io/about.html"); return false; });
-$('#help').click(function(){ setLink("http://itrolledu.github.io/help.html"); return false; });
+$('#home').click(function(){ setLink("main.html"); return false; });
+$('#mac').click(function(){ setLink("index.html"); return false; });
+$('#win').click(function(){ setLink("index.html"); return false; });
+$('#linux').click(function(){ setLink("index.html"); return false; });
+$('#about').click(function(){ setLink("about.html"); return false; });
+$('#help').click(function(){ setLink("help.html"); return false; });
 $.ajax({
     type: "GET",
-    url: "http://itrolledu.github.io/main.html",
+    url: "main.html",
     dataType: "jsonp",
 }).success( function( data ) {
-    $( 'div.ajax-field' ).html( data );
+    $( '#siteloader' ).html( data );
 });
