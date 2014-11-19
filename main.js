6c945cee47861206271506dadb1ad255f0512441
Mason X <ohnx@users.noreply.github.com>
2014-11-18 20:59:49 -0500

    texting

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function setLink(url) {
-  	$("#siteloader").html('<object data="'+url+'" height="90%" width="100%">');
+  	$("#siteLoader").load(url);
+  	//$("#siteloader").html('<object data="'+url+'" height="90%" width="100%">');
 }
-$('#home').click(function(){ setLink("http://itrolledu.github.io/main.html"); return false; });
-$('#mac').click(function(){ setLink("http://itrolledu.github.io/mac/index.html"); return false; });
-$('#win').click(function(){ setLink("http://itrolledu.github.io/win/index.html"); return false; });
-$('#linux').click(function(){ setLink("http://itrolledu.github.io/linux/index.html"); return false; });
-$('#about').click(function(){ setLink("http://itrolledu.github.io/about.html"); return false; });
-$('#help').click(function(){ setLink("http://itrolledu.github.io/help.html"); return false; });
-$("#siteloader").html('<object data="http://itrolledu.github.io/main.html" height="90%" width="100%">');
+$('#home').click(function(){ setLink("/main.html"); return false; });
+$('#mac').click(function(){ setLink("/mac/index.html"); return false; });
+$('#win').click(function(){ setLink("/win/index.html"); return false; });
+$('#linux').click(function(){ setLink("/linux/index.html"); return false; });
+$('#about').click(function(){ setLink("/about.html"); return false; });
+$('#help').click(function(){ setLink("/help.html"); return false; });
+$("#siteLoader").load("/main.html");
