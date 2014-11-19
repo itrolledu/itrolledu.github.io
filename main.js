17e4bca10b062775f6c8052545f475fbd7a6c9cf
Mason X <ohnx@users.noreply.github.com>
2014-11-18 21:13:51 -0500

    try ajax
    objects dont work 100%

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-function setLink(url) {
-  	$("#siteLoader").load(url);
-  	//$("#siteloader").html('<object data="'+url+'" height="90%" width="100%">');
+function setLink(page) {
+$.ajax({
+    type: "GET",
+    url: page,
+    dataType: "jsonp",
+}).success( function( data ) {
+    $( 'div.siteloader' ).html( data );
+});
 }
-$('#home').click(function(){ setLink("/main.html"); return false; });
-$('#mac').click(function(){ setLink("/mac/index.html"); return false; });
-$('#win').click(function(){ setLink("/win/index.html"); return false; });
-$('#linux').click(function(){ setLink("/linux/index.html"); return false; });
-$('#about').click(function(){ setLink("/about.html"); return false; });
-$('#help').click(function(){ setLink("/help.html"); return false; });
-$("#siteLoader").load("/main.html");
+$('#home').click(function(){ setLink("http://itrolledu.github.io/main.html"); return false; });
+$('#mac').click(function(){ setLink("http://itrolledu.github.io/mac/index.html"); return false; });
+$('#win').click(function(){ setLink("http://itrolledu.github.io/win/index.html"); return false; });
+$('#linux').click(function(){ setLink("http://itrolledu.github.io/linux/index.html"); return false; });
+$('#about').click(function(){ setLink("http://itrolledu.github.io/about.html"); return false; });
+$('#help').click(function(){ setLink("http://itrolledu.github.io/help.html"); return false; });
+$.ajax({
+    type: "GET",
+    url: "http://itrolledu.github.io/main.html",
+    dataType: "jsonp",
+}).success( function( data ) {
+    $( 'div.ajax-field' ).html( data );
+});
